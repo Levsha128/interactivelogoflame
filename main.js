995e994991ae8560a04157ee5b7a89c372aafe11
Alexey Shulga <levsha128@gmail.com>
2019-05-07 01:32:08 +0300

    Add intensity and sin draw

--- main.js
+++ main.js
@@ -17,25 +17,103 @@ drawCircle = (x, y, r) => {
 };
 
 // RANDOM
-drawRandom = () => {
-  let x_c = 77;
-  let y_c = 45;
-  let x_step = 10;
-  let y_step = 10;
-  let x_offset = 20;
-  let y_offset = 20;
-  let base_r = 3;
-  for (let i = 0; i < x_c; i++) {
-    for (let j = 0; j < y_c; j++) {
-      let r = base_r * Math.random();
-      drawCircle(x_offset + i * x_step, y_offset + j * y_step, r);
-    }
-  }
-};
-
-
-setInterval(() => {
-  clear();
-  drawRandom();
-}, 300);
+// drawRandom = () => {
+//   let xC = 77;
+//   let yC = 45;
+//   let xStep = 10;
+//   let yStep = 10;
+//   let xOffset = 20;
+//   let yOffset = 20;
+//   let baseR = 3;
+//   for (let i = 0; i < xC; i++) {
+//     for (let j = 0; j < yC; j++) {
+//       let r = baseR * Math.random();
+//       drawCircle(xOffset + i * xStep, yOffset + j * yStep, r);
+//     }
+//   }
+// };
+//
+//
+// setInterval(() => {
+//   clear();
+//   drawRandom();
+// }, 300);
 //END RANDOM
+
+
+
+//INTENSIVITY
+// drawIntensivity = (posX, posY, positive) => {
+//   let xC = 77;
+//   let yC = 45;
+//   let step = 10;
+//   let xOffset = 20;
+//   let yOffset = 20;
+//   let baseR = 3;
+//   let maxR = 8;
+//   let minR = 0;
+//   for (let i = 0; i < xC; i++) {
+//     for (let j = 0; j < yC; j++) {
+//       let x = xOffset + i * step;
+//       let y = yOffset + j * step;
+//       let r = baseR + (positive ? 1: -1) * (1 / Math.sqrt((x-posX)*(x-posX)+(y-posY)*(y-posY))) * 200;
+//
+//       if (r > maxR) {
+//         r = maxR;
+//       } else if (r < minR) {
+//         r = minR;
+//       }
+//       drawCircle(x, y, r);
+//     }
+//   }
+// };
+//
+//
+// canvas.addEventListener('mousemove', (event) => {
+//   const bounds = canvas.getBoundingClientRect();
+//   const posX = event.clientX - bounds.left;
+//   const posY = event.clientY - bounds.top;
+//   // console.log(posX, posY);
+//   clear();
+//   drawIntensivity(posX, posY, true);
+//   }
+// );
+//END INTENSIVITY
+
+
+//SIN
+// drawSin = (counter) => {
+//   let xC = 77;
+//   let yC = 45;
+//   let step = 10;
+//   let xOffset = 20;
+//   let yOffset = 20;
+//   let baseR = 3;
+//   let maxR = 6;
+//   let minR = 2;
+//   for (let i = 0; i < xC; i++) {
+//     for (let j = 0; j < yC; j++) {
+//       let x = xOffset + i * step;
+//       let y = yOffset + j * step;
+//       let r = baseR +
+//         Math.cos(counter / 20 + i/8) * 2 +
+//         Math.sin(counter / 20 + j/8) * 2
+//
+//       ;
+//
+//       if (r > maxR) {
+//         r = maxR;
+//       } else if (r < minR) {
+//         r = minR;
+//       }
+//       drawCircle(x, y, r);
+//     }
+//   }
+// };
+// let counter = 0;
+// setInterval(() => {
+//   clear();
+//   drawSin(++counter);
+// }, 20);
+//END SIN
+
